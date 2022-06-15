d52ba08533984d71c6064856adc1ff8e27df137d
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-06-15 15:15:02 +0530

    Time: 146 ms (83.67%), Space: 93.3 MB (52.21%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,14 @@
-
-
 class Solution {
 public:
-    // Time Complexity:- O(N)
-    // Space Complexity:- O(1)
+    
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int min_buy_cost = prices[0],max_profit = 0;
-        for(int i=1;i<n;i++){
-            // sell the stock now
-            max_profit = max(max_profit,prices[i]-min_buy_cost);
-            
-            // buy the stock now
-            min_buy_cost = min(min_buy_cost,prices[i]);
+        int maxi=0;
+        for(int x=prices.size()-1;x>=0;x--)
+        {
+            maxi=max(maxi,prices[x]);
+            prices[x]=abs(prices[x]-maxi);    
         }
-        return max_profit;
+        
+        return *max_element(prices.begin(),prices.end());
     }
 };

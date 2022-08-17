5cd32a9cf8be1d2eb6d462523df4f49d215596b0
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-08-18 02:55:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (69.65%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,16 +1,19 @@
-class Solution {
+/*class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int n=s.size();
         int m=t.size();
-        int res=solve(s,t,n,m);
-        if(res!=n)
+        if (n > m) {
+        return false;
+    }
+        
+        if(solve(s,t,n,m)!=n)
             return false;
         else
             return true;
     }
     
-    int solve(string X,string Y,int n,int m)
+    int solve(string &X,string &Y,int n,int m)
     {
         if(n==0||m==0)
             return 0;
@@ -23,5 +26,35 @@ public:
         {
             return solve(X,Y,n,m-1);
         }
+    }
+};*/
+
+class Solution {
+    int isSubSequence(string& s1, string& s2, int i, int j)
+{
+    if (i == 0 || j == 0) {
+        return 0;
+    }
+    if (s1[i - 1] == s2[j - 1]) {
+        return  1 + isSubSequence(s1, s2, i - 1, j - 1);
+    }
+    else {
+        return  isSubSequence(s1, s2, i, j - 1);
+    }
+}
+ 
+public:
+    bool isSubsequence(string s, string t) {
+         int m = s.size();
+    int n = t.size();
+    if (m > n) {
+        return false;
+    }
+    if (isSubSequence(s, t, m, n) == m) {
+        return true;
+    }
+    else {
+        return false;
+    }
     }
 };

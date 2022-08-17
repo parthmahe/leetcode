3085763c169d756074ab887fe2f7bd64359e7ab8
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-08-18 02:00:58 +0530

    Time: 3 ms (41.47%), Space: 6.2 MB (53.43%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
+        if(n==0 || n==1)
+            return n;
+        
         vector<int>dp(n+1);
         dp[0]=0;
         dp[1]=1;
+        int maxi=1;
         for(int x=2;x<dp.size();x++)
         {
             if(x%2)
@@ -18,7 +18,8 @@ public:
             {
                 dp[x]=dp[x/2];
             }
+            maxi=max(maxi,dp[x]);
         }
-        return *max_element(dp.begin(),dp.end());
+        return maxi;
     }
 };

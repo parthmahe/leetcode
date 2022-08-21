5a6b7eeb1338740c53d019ccbcbefe04cb9c9776
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-08-22 02:01:29 +0530

    Time: 163 ms (58.87%), Space: 83.5 MB (5.95%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum=nums[0];
-        int curSum=0;
+        vector<vector<int>>dp(2,vector<int>(nums.size()+1,-1));
+        return maxsum(nums,0,false,dp);
+    }
+    
+    int maxsum(vector<int>&nums,int i,bool mustpick,vector<vector<int>>&dp)
+    {
+        if(i==nums.size())
+            return mustpick?0:-1e5;
         
-        for(int i=0;i<nums.size();i++)
-        {
-            if(curSum<0)
-                curSum=0;
-            curSum+=nums[i];
-            maxSum=max(maxSum,curSum);
-        }
+        if(dp[mustpick][i]!=-1)
+            return dp[mustpick][i];
         
-        return maxSum;
+        if(mustpick)
+            return dp[mustpick][i]=max(0,nums[i]+maxsum(nums,i+1,true,dp));
+        
+        return dp[mustpick][i]=max(nums[i]+maxsum(nums,i+1,true,dp),maxsum(nums,i+1,false,dp));
         
     }
 };

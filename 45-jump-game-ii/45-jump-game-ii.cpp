620743341ab09805ca40d68558d476c6eb9d870f
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-09-04 13:26:50 +0530

    Time: 419 ms (35.97%), Space: 22.6 MB (6.38%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int>dp(1e4+1,-1);
+        
+        return jumpcount(nums,0,dp);
+        
+    }
+    
+    int jumpcount(vector<int>&nums,int i,vector<int>&dp)
+    {
+        if(i>=nums.size()-1)
+            return 0;
+      
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int x=nums[i];
+        if(x==0)
+            return 1e9;
+        
+        int res= INT_MAX;
+        while(x--)
+        {
+            // int temp;
+            // if(dp[i+x+1]!=-1)
+            //     temp=dp[i+x+1];
+            // else
+            // {   temp=1+jumpcount(nums,i+x+1,dp);
+            //     dp[i+x+1]=temp;
+            // }
+            
+            res=min(res,1+jumpcount(nums,i+x+1,dp));
+        }
+        
+        return dp[i]=res;
+        
+    }
+    
+    
+};

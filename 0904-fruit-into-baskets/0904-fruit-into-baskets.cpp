1771c40c0f26463e68d7a6ade24a166c5c469577
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2022-11-02 14:03:35 +0530

    Time: 365 ms (49.39%), Space: 75.3 MB (5.52%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int>mp;
+        map<int,int>mp;
         int res=0;
         int i=0;
         int j=0;

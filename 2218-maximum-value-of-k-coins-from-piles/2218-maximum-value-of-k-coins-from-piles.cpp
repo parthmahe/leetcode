9cbaee7187039d1cb944f41382b56abb515f5fb9
Parth Maheshwari <91911163+parthmahe@users.noreply.github.com>
2023-04-16 23:40:09 +0530

    Time: 340 ms (40.21%), Space: 18.1 MB (66.06%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -28,47 +28,33 @@ public:
             
         }
     
-    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+    int maxValueOfCoins(vector<vector<int>>& piles, int maxCoins) {
         int n=piles.size();
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return solve(0,k,piles,dp);
+        vector<vector<int>>dp(n+1,vector<int>(maxCoins+1,0));
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int k=1;k<=maxCoins;k++)
+            {
+                int currPilesize=piles[idx].size();
+                int maxCanpick=min(k,currPilesize);
+
+
+                int notpick=dp[idx+1][k];
+                int pickedsum=0;
+
+                int finalsum=notpick;
+                for(int i=0;i<maxCanpick;i++)
+                {
+                    pickedsum+=piles[idx][i];
+                    int pick=pickedsum+dp[idx+1][k-i-1];
+                    finalsum=max(finalsum,pick);
+                }
+
+                dp[idx][k]=finalsum;
+            
+            }
+        }
+        
+        return dp[0][maxCoins];
     }
 };
-
-// class Solution {
-// public:
-//     //startPileIdx is always 0
-//     //piles under consideration for each call ==> [pileIdx = 0 to pileIdx = endPileIdx]
-//     int solve(vector<vector<int>>&piles, vector<vector<int>>&dp, int endPileIdx, int k)
-//     {
-//         if (endPileIdx < 0 || k == 0) return 0;
-//         if (dp[endPileIdx][k] != -1) return dp[endPileIdx][k];
-        
-//         int currPileSize = piles[endPileIdx].size(); 
-//         int maxCanPick = min(k, currPileSize);
-        
-//         //consider 'not picking' any coin
-//         int maxSum = solve(piles, dp, endPileIdx - 1, k);  //DON'T PICK ANY COIN FROM CURR PILE
-//         int pickedSum = 0;
-//         //===============================================================================
-//         //now consider picking some coins
-//         for (int i = 0; i < maxCanPick; i++)
-//         {
-//             int coinValue = piles[endPileIdx][i];
-//             pickedSum += coinValue;
-            
-//             int nextMaxSum = solve(piles, dp, endPileIdx - 1, k - i - 1); 
-//             maxSum = max(maxSum, pickedSum + nextMaxSum);
-//         }
-//         //=================================================================================
-//         return dp[endPileIdx][k] = maxSum;
-        
-//     }
-//     int maxValueOfCoins(vector<vector<int>>& piles, int k) 
-//     {
-//         int n = piles.size();
-//         vector<vector<int>>dp(n, vector<int>(k + 1, -1));
-//         int ans = solve(piles, dp, n - 1, k);
-//         return ans;
-//     }
-// };
